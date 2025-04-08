d7e8ff65f5546a382712c0b947765c4d42966196
Alex Nascimento <137247689+alexnascimento1980@users.noreply.github.com>
2025-04-07 22:25:46 -0300

    implementando autenticação com github

--- setup/settings.py
+++ setup/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
+
 
 # Application definition
 
@@ -141,8 +143,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SOCIALACCOUNT_PROVIDERS = {
     'github': {
         'APP': {
-            'client_id': '123',
-            'secret': '456',
+            'client_id': 'Ov23cttav8Pk7yYecMgE',
+            'secret': 'dedea52265de523c63c228633fb27c9c3a18bd4d',
             'key': ''
         }
     }
